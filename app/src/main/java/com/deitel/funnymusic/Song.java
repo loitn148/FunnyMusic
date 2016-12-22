bcf6beeefca80d6e261133d814cd8da1012ebac3
Trinh Ngoc Loi <loitn148@gmail.com>
2016-12-22 19:53:30 +0700

    Change#2

--- app/src/main/java/com/deitel/funnymusic/Song.java
+++ app/src/main/java/com/deitel/funnymusic/Song.java
@@ -15,13 +15,13 @@ public class Song {
     private int duration;
 
     public Song(String id, String name, String title, String album, String artist, String path, int duration) {
-        //this.id = id;
-        //this.name = name;
+        this.id = id;
+        this.name = name;
         this.title = title;
         this.album = album;
-        this.artist = artist;
-        this.path = path;
-        this.duration = duration;
+        //this.artist = artist;
+        //this.path = path;
+        //this.duration = duration;
     }
 
     public String getId() {
